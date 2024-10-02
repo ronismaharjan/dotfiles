2e445c2751785714064ccc6d31be35025b55eff0
ronish maharjan <ronishmaharjan137@gmail.com>
2024-10-02 23:46:55 +0545

    nvim dotfile added

---
+++ nvim/init.lua
@@ -0,0 +1,4 @@
+require('keymaps')
+require('lazypackagemanager')
+require('settings')
+vim.cmd("colorscheme habamax")
---
+++ nvim/lua/settings.lua
@@ -0,0 +1,17 @@
+-- General settings
+vim.opt.number = true        -- Show line numbers
+vim.opt.relativenumber = true -- Show relative line numbers
+vim.opt.expandtab = true      -- Convert tabs to spaces
+vim.opt.shiftwidth = 4        -- Number of spaces to use for autoindent
+vim.opt.tabstop = 4           -- Number of spaces in a tab
+vim.opt.smartindent = true    -- Smart indentation
+vim.opt.hlsearch = true       -- Highlight search results
+vim.opt.incsearch = true      -- Incremental search
+vim.opt.ignorecase = true     -- Case insensitive searching
+vim.opt.smartcase = true      -- Case sensitive if uppercase is present
+vim.opt.wrap = false          -- Disable line wrapping
+vim.opt.backup = false        -- Disable backup file creation
+vim.opt.writebackup = false   -- Disable write backup file creation
+vim.opt.swapfile = false      -- Disable swap file creation
+vim.opt.mouse = "v"           -- Make mouse usable only in visual mode
+vim.opt.showmatch = true
