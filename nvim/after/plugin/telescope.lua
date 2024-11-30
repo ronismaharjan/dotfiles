56c2d4c26bb231c2be5d33cc140c9040dea9e3c5
ronish <ronishmaharjan137@gmail.com>
2024-11-30 16:24:17 +0545

    enhanced version

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,1 +1,7 @@
-require("telescope").setup({})
+require("telescope").setup({
+  defaults = {
+    -- Disable history for all pickers
+    history = false,  -- Disables history for all Telescope commands
+    file_ignore_patterns = {"node_modules/*"},  -- Optional: customize file ignoring
+  },
+})
